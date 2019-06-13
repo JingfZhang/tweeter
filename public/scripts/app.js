2fe6bdc12f1d7765b3ff974fa98588a6c0714973
JingfZhang <zhangjingfan.1996@gmail.com>
2019-06-13 19:14:21 +0000

    comments added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,10 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+//function that create each tweet
 function createTweetElement(tweetData) {
+  //create each necessary component for each tweet and add class when needed.
   var $tweet = $("<article>").addClass("tweet");
   var $header = $("<header>");
   var $name = $("<span>").addClass("name");
@@ -12,6 +15,7 @@ function createTweetElement(tweetData) {
   var $tweetMessage = $("<p>");
   var $footer = $("<footer>");
 
+  //Combine the content and components in order to form an entire "tweet".
   $name.append(tweetData["user"]["name"]);
   $handler.append(tweetData["user"]["handle"]);
   $header.append("<img src=\"" + tweetData["user"]["avatars"]["small"] + "\">");
@@ -29,6 +33,7 @@ function createTweetElement(tweetData) {
   return $tweet;
 }
 
+//function that render each tweet using createTweetElement function
 function renderTweets(tweets) {
   for (let tweet of tweets) {
     $("#tweets").prepend(createTweetElement(tweet));
@@ -36,20 +41,25 @@ function renderTweets(tweets) {
 }
 
 $(document).ready(function() {
+  //renders the exsiting tweets when loading page
   $.get("/tweets", function(data) {renderTweets(data)});
 
+  //slide up the compose section before user click "Compose" button.
   $("section.new-tweet").slideUp();
 
+  //When "Compose" button is clicked toggle the compose section, and auto select textarea when compose section slides down.
   $("button.compose").click(function() {
       $("section.new-tweet").slideToggle(function() {
         $("#tweet-message").select();
       });
   })
 
-
+  //What to do after clicking "Tweet" button.
   $("form").on("submit", function(event) {
+    //stop page from refreshing after "Tweet" is clicked.
     event.preventDefault();
 
+    //If nothing is entered to textarea show error message to remind user.
     if ($("#tweet-message").val() === "") {
       $(".new-tweet p.error-message").slideDown().text("Cannot tweet empty tweets!");
 
@@ -57,6 +67,7 @@ $(document).ready(function() {
         $(".new-tweet p.error-message").slideUp();
       });
 
+    // If more than 140 words is entered show error message to remind user.
     } else if ($(".counter").html() < 0) {
       $(".new-tweet p.error-message").slideDown().text("Cannot compose more than 140 words!");
 
@@ -64,6 +75,7 @@ $(document).ready(function() {
         $(".new-tweet p.error-message").slideUp();
       });
 
+    //If everything entered is valid update message to database and render new tweet to the page.
     } else {
       var value = $("#tweet-message").val();
 
